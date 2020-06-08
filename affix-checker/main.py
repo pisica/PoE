2ec7d12f0815c91178c6f7cab76f4527c023d1ba
darkcloud <bpisica@gmail.com>
2020-06-07 19:54:51 -0700

    parse json and print item affixes

--- affix-checker/main.py
+++ affix-checker/main.py
@@ -1,20 +1,31 @@
 import requests
+import json
 
-def getLadder(limit, offset):
-	queryString = 'https://www.pathofexile.com/api/ladders/Delirium?limit={}&offset={}&type=league'.format(limit, offset)
-	print(queryString)
-	r = requests.get(queryString)
-	return r.text
+def get_ladder(limit, offset):
+	query_string = 'https://www.pathofexile.com/api/ladders/Delirium?limit={}&offset={}&type=league'.format(limit, offset)
+	print(query_string)
+	r = requests.get(query_string)
+	return r.json()
 
-def getCharacter(accountName, character):
-	queryString = 'https://www.pathofexile.com/character-window/get-items?accountName={}&character={}'.format(accountName, character)
-	print(queryString)
-	r = requests.get(queryString)
-	return r.text
+def get_character(accountName, character):
+	query_string = 'https://www.pathofexile.com/character-window/get-items?accountName={}&character={}'.format(accountName, character)
+	print(query_string)
+	r = requests.get(query_string)
+	return r.json()
 
 def main():
-	print(getLadder(200, 0))
-	print(getCharacter('entreri0', 'DarkcloudZS'))
+	# print(get_ladder(200, 0))
+	character = get_character('entreri0', 'DarkcloudZS')
+	# print(character['items'][0]['frameType'])
+	for item in character['items']:
+		if item['name']:
+			print('{}: {}'.format(item['inventoryId'], item['name']))
+		else:
+			print('{}: {}'.format(item['inventoryId'], item['typeLine']))
+
+		for mod in item['explicitMods']:
+			print(mod)
+		print('')
 
 if __name__ == '__main__':
 	main()
